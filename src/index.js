f3d622da02e15e9d27cfc2e26a48c349f8fef8bb
Alinovskaya Anna <an1tachka21@gmail.com>
2020-02-20 12:22:55 +0300

    typical-arrays-problems task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
 
 exports.min = function min (array) {
-  return 0;
+    if(array === undefined || array.length < 1 ){
+        return 0;
+    }
+    else return Math.min.apply(null,array);
 }
 
 exports.max = function max (array) {
-  return 0;
+    if(array === undefined || array.length < 1 ){
+        return 0;
+    } 
+    else return Math.max.apply(null,array);
 }
 
 exports.avg = function avg (array) {
-  return 0;
+    if(array === undefined || array.length < 1 ){
+        return 0;
+    }
+    else return array.reduce((sum,item) => sum += item) / array.length;
 }
